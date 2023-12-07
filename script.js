b97c658f3651c929cff3a09249570a4233d7b0dd
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-07 13:44:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ function isSameType(value1, value2) {
     return true;
   }
 
-  // Check if the types are the same
-  return typeof value1 === typeof value2;
+  // Check if the types are the same and not NaN
+  return typeof value1 === typeof value2 && !isNaN(value1) && !isNaN(value2);
 }
 
 // Examples
-console.log(isSameType(5, 10));            // true
-console.log(isSameType('hello', 'world')); // true
-console.log(isSameType(3, '3'));            // false
-console.log(isSameType(NaN, NaN));          // true 
-	
+console.log(isSameType(1, 3));           // true
+console.log(isSameType("hey", "hello")); // true
+console.log(isSameType(NaN, NaN));        // true
+console.log(isSameType("3", 3));          // false
 
 }
 
